eaa072b07d3a81a20e76bb7bcbf8fcdacdde8515
Ibrahim Kurhan <ikurhan@outlook.com>
2020-07-30 12:53:49 +0100

    info has been passed to the firebase

--- src/Component/Logout/LoggedIn.jsx
+++ src/Component/Logout/LoggedIn.jsx
@@ -4,6 +4,8 @@ import { Button } from "semantic-ui-react";
 
 const Logout = () => {
   const [names, setNames] = useState("");
+  // const [input, setInput] = useState("");
+  const [users, setUsers] = useState("");
 
   useEffect(() => {
     db.collection("names").onSnapshot((snapshot) => {
@@ -13,6 +15,14 @@ const Logout = () => {
   }, []);
   const signOut = () => {
     firebase.auth().signOut();
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    db.collection("users").add({
+      names: users,
+    });
+    setUsers("");
   };
 
   return (
@@ -25,12 +35,17 @@ const Logout = () => {
       <Button
         style={{ width: "150px", height: "35px" }}
         primary
-        // className={style.Button}
         onClick={signOut}
       >
         Logout
       </Button>
+      <h2>This message is coming from firebase database</h2>
       <p>{names}</p>
+      <h2>
+        The information you will enter will be stored in firebase database
+      </h2>
+      <input type="text" onChange={(e) => setUsers(e.target.value)} />
+      <button onClick={(e) => submit(e)}>Add info</button>
     </div>
   );
 };
