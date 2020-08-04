ade530c3d1621779a19a6c19c917e069ac2ec5f3
Ibrahim Kurhan <ikurhan@outlook.com>
2020-08-04 23:27:53 +0100

    new test added to app component

--- src/Container/App.test.js
+++ src/Container/App.test.js
@@ -2,9 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
 import App from "./App.js";
+import LoggedIn from "../Component/LoggedIn/LoggedIn";
+import { NavLink } from "react-router-dom";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+describe("<App />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it("should render  <LoggedIn /> if user loggedIn", () => {
+    expect(wrapper.find(LoggedIn));
+  });
+  it("should render  <NavLink /> if user is not loggedIn", () => {
+    expect(wrapper.find(NavLink)).toHaveLength(2);
+  });
 });
